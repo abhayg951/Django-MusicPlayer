12591d8d40515e26eb8b66f3b1f1c342fe2ff57b
Abhay Gupta <109977125+abhayg951@users.noreply.github.com>
2023-11-04 14:26:55 +0530

    final edit

--- core/helper.py
+++ core/helper.py
@@ -1,6 +1,14 @@
 from mutagen.mp3 import MP3
+from mutagen.id3 import ID3
 
 
 def get_music_length(file):
     music = MP3(file)
     return music.info.length
+
+
+def get_music_title(file):
+    print(file)
+    file_name = str(file)
+    print(file_name.rstrip(".mp3"))
+    return str(file_name.rstrip(".mp3"))
--- core/models.py
+++ core/models.py
@@ -1,22 +1,29 @@
 from django.db import models
-from core.helper import get_music_length
+from core.helper import get_music_length, get_music_title
 from core.validator import audio_validation
 
 
 # Create your models here.
 
 class Music(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, blank=True)
     artist = models.CharField(max_length=200)
-    album = models.ForeignKey("Album", on_delete=models.SET_NULL, null=True, blank=True)
-    time_length = models.DecimalField(decimal_places=2, max_digits=20, blank=True)
-    audio_file = models.FileField(upload_to='musics', validators=[audio_validation])
+    album = models.ForeignKey(
+        "Album", on_delete=models.SET_NULL, null=True, blank=True)
+    time_length = models.DecimalField(
+        decimal_places=2, max_digits=20, blank=True)
+    audio_file = models.FileField(
+        upload_to='musics', validators=[audio_validation])
     cover_image = models.ImageField(upload_to='cover_images/')
 
     def save(self, *args, **kwargs):
         if not self.time_length:
             music_length = get_music_length(self.audio_file)
             self.time_length = music_length
+
+            print("This is the running")
+            music_title = get_music_title(self.audio_file)
+            self.title = music_title
         return super().save(*args, **kwargs)
 
     def __str__(self):
